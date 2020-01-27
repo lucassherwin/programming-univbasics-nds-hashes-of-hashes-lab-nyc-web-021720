2928cf01c11f90aa4e57587b505fbc6c86dce7dd
Lucas Sherwin <lksherwin@gmail.com>
2020-01-27 21:09:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -61,7 +61,7 @@ def naming_system
               sub_category:
               {
                 label: "Species"
-                sub_category: nil
+                sub_category: {}
               }
             }
           }
